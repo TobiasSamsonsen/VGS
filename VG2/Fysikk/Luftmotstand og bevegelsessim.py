2edfc3b405333ca1f5db4773ffa7bef8e60e672e
Tobias Samsonsen <Tobias.samsonsen@gmail.com>
2023-10-20 10:37:59 +0200

    luftmotstand sim

---
+++ VG2/Fysikk/Luftmotstand og bevegelsessim.py
@@ -0,0 +1,40 @@
+from matplotlib import pyplot as plt
+
+m = 75
+g = 9.81
+cd = 0.7
+
+s = 0
+v = 0
+t = 0
+G = m*g
+
+def a(v):
+    D = cd*(v**2)
+    sum_F = G-D
+    aks = sum_F/m
+    return aks
+
+t_end = 40
+dt = 0.01
+s_values = [s]
+v_values = [v]
+a_values = [a(v)]
+t_values = [t]
+
+while s < 417:
+    s += v*dt
+    v += a(v)*t
+    t += dt
+    
+    s_values.append(s)
+    v_values.append(v)
+    t_values.append(t)
+    a_values.append(a(v))
+    
+plt.plot(t_values, s_values, label="Strekning")
+plt.plot(t_values, v_values, label="Fart")
+plt.plot(t_values, a_values, label="Akselerasjon")
+plt.grid()
+plt.legend()
+plt.show()
