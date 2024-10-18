e1937f846f26ab2250b8f33cba4f3052793d5935
Tobias Samsonsen <116917837+TobiasSamsonsen@users.noreply.github.com>
2024-10-18 22:15:22 +0200

    Fix

--- VG3/MAT100/Newton-Raphson.py
+++ VG3/MAT100/Newton-Raphson.py
@@ -8,17 +8,10 @@ def df(x):
     return (f(x+h) - f(x))/h
 
 def newton_raphson(a, b, accuracy):
-    x = a
-    
-    
-    while (x - f(x)/df(x)) < a or (x - f(x)/df(x)) > b:
-        x += 0.01
+    x = (a+b)/2
     
     while abs(f(x)) > accuracy:
-        if f(a)*f(x) < 0:
-            x = x + f(x)/df(x)
-        else:
-            x = x - f(x)/df(x)
+        x -= f(x)/df(x)
         
     return x
 
@@ -27,9 +20,10 @@ h = 1E-7
 # Oppgave C
 # Vis, ved å bruke skjæringssetningen, at denne ligningen har tre løsninger, ett i hvert av intervallene [-5, -1], [-1, 1] og [1, 2]
 
-print(f"f(-5) = {f(-5)}, f(-1) = {f(-1)}, f-verdiene har forskjellig fortegn, så det er en skjæring i intervallet [-5, -1]")
-print(f"f(-1) = {f(-1)}, f(1) = {f(1)}, f-verdiene har forskjellig fortegn, så det er en skjæring i intervallet [-1, 1]")
-print(f"f(1) = {f(1)}, f(2) = {f(2)}, f-verdiene har forskjellig fortegn, så det er en skjæring i intervallet [1, 2]")
+print(f"f(-5) = {round(f(-5), 6)}, f(-1) = {round(f(-1), 6)}")
+print(f"f(-1) = {round(f(-1), 6)}, f(1) = {round(f(1), 6)}")
+print(f"f(1) = {round(f(1), 6)}, f(2) = {round(f(2), 6)}")
+print(f"\nVi ser at i alle intervallene er det forskjellig fortegn mellom f(a) og f(b), \ndermed har ligningen, i følge skjæringssetningen, en løsning i hvert av intervallene [-5, -1], [-1, 1] og [1, 2]")
 
 
 print("------------------------------------")
@@ -39,6 +33,8 @@ Bruk Newton-Raphsons metode og velg passende startverdi for de tre forskjellige 
 Hvilke startverdier dere valgte må komme fram av besvarelsen deres.
 OBS: Pass på å ikke bruk ekstremalverdier som startverdi! (hvorfor ikke?) '''
 
-print(f"Skjæring ved x-akse: x = {round(newton_raphson(-5, -1, 1E-6), 6)}")
-print(f"Skjæring ved x-akse: x = {round(newton_raphson(-1, 1, 1E-6), 6)}")
-print(f"Skjæring ved x-akse: x = {round(newton_raphson(1, 2, 1E-6), 6)}")
+print(f"Skjæring ved x-akse i intervallet [-5, -1]: x = {round(newton_raphson(-5, -1, 1E-6), 6)}")
+print(f"Skjæring ved x-akse i intervallet [-1, 1]: x = {round(newton_raphson(-1, 1, 1E-6), 6)}")
+print(f"Skjæring ved x-akse i intervallet [1, 2]: x = {round(newton_raphson(1, 2, 1E-6), 6)}")
+
+print(f"\nJeg valgte startverdiene til å være midtpunktet i intervallet.\nDette fordi det minsker sjansen for at metoden divergerer.\nJeg opplevde at metoden divergerte når jeg valgte a som startverdi i intervallene [a, b], siden disse var i nærheten av ekstremalpunkter")
